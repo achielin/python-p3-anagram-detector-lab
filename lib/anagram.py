c03fbdb2d4f9fa63773c31a6abff302b151b3435
linder Opondo <achielinder@gmail.com>
2023-12-01 12:53:26 +0300

    completed assignments

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word, word_list=None):
+        self.word = word
+        self.word_list = word_list if word_list != None else list()
+    def match(self, word_list=None):
+        word_list = word_list if word_list != None else self.word_list
+        anagram_list = list()
+
+        for word in word_list:
+            if (len(word) == len(self.word)) and (sorted(word) == sorted(self.word)):
+                anagram_list.append(word)
+        return anagram_list
